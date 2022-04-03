474d85d4135d48e94c9b4b7f7b9d93ea6e05b2a6
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-03 00:02:24 -0230

    disaster.py

---
+++ chapter4/double_if.py
@@ -0,0 +1,12 @@
+furry = True
+large = True
+if furry:
+    if large:
+        print("It's a yeti.")
+    else:
+        print("It's cat!")
+else:
+    if large:
+        print("It's whale!")
+    else:
+        print("I know it was a human.")
