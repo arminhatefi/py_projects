c1e25f0ec3b253899a1500a85b01890996a23c5e
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-02 23:51:23 -0230

    disaster.py

---
+++ chapter4/continue.py
@@ -0,0 +1,21 @@
+sum = 0
+sum += 1
+sum += 2
+sum += 3
+sum += 4
+print(sum)
+
+sum2 = 1 + \
+    2 + \
+    3 + \
+    5
+
+print(sum2)
+
+sum3 = (
+    1 +
+    2 +
+    3 +
+    9
+)
+print(sum3)
