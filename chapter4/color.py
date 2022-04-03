f4bed642c2347efe18c9efa2ffb86563c04e63cf
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-03 00:12:00 -0230

    color added

---
+++ chapter4/color.py
@@ -0,0 +1,10 @@
+color = "mauve"
+if color == "red":
+    print("It's a tomato")
+elif color == "green":
+    print("It's a green pepper")
+elif color == "bee purple":
+    print("I do not know what color it is, but only bee can see it")
+else:
+    print("I've never heard of the color", color)
+
