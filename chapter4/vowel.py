a2e6ea8573e3324725247b415cb504e0cb5d055a
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-03 00:39:54 -0230

    git_py

---
+++ chapter4/vowel.py
@@ -0,0 +1,8 @@
+letter = 'o'
+if letter == 'a' or letter == 'e' or letter == 'i'\
+    or letter == 'o' or letter == 'u':
+    print(letter, "is a vowel")
+else:
+    print(letter, "os not a vowel")
+
+    
---
+++ chapter4/vowel2.py
@@ -0,0 +1,7 @@
+vowels = 'aeiou'
+letter = 'o'
+if letter in vowels:
+    print(letter, ", Good job")
+else:
+    print(letter,", bad job")
+
