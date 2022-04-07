e57fea4acf2e6630767f6092295a578c97c1b827
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-06 23:24:38 -0230

    commit 1

---
+++ chapter5/case.py
@@ -0,0 +1,11 @@
+setup = 'a duck goes into a bar....'
+print(setup.strip('.'))
+print(setup.capitalize())
+print(setup.title())
+print(setup.upper())
+print(setup.lower())
+
+thing = 'wereduck'
+place = 'werepond'
+
+print(f'The {thing} is in the {place}')
