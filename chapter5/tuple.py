f92c75c5f46a2c971cc5f4cc5ced59e5a7b3dc46
Armin Hatefi <hatefi.ar@gmail.com>
2022-04-10 23:54:16 -0230

    commit 1

--- chapter5/tuple.py
+++ chapter5/tuple.py
@@ -34,3 +34,55 @@ marx_tuple1.append('Zeppo')
 
 marx_tuple1.insert(2, 'Gummo')
 print(marx_tuple1)
+
+marx_tuple2 = ['Gummo', 'Karl']
+marx_tuple1.extend(marx_tuple2)
+print(marx_tuple1)
+
+marx_tuple3 = ['Adam', 'Misa']
+marx_tuple1 += marx_tuple3
+print(marx_tuple1)
+
+marx_tuple3[1] = 'Wanda'
+print(marx_tuple3)
+
+numbers = [1, 2, 3, 4]
+numbers[1:3] = [8,9]
+print(numbers)
+
+numbers = [1, 2, 3, 4]
+numbers[1:3] = [7, 8, 9]
+print(numbers)
+
+numbers = [1, 2, 3, 4]
+numbers[1:3] = []
+print(numbers)
+
+numbers = [1, 2, 3, 4]
+numbers[1:3] = (98, 99, 100)
+print(numbers)
+
+numbers = [1, 2, 3, 4]
+numbers[1:3] = 'wat?'
+print(numbers)
+
+print(marx_tuple1[-1])
+del marx_tuple1[-1]
+print(marx_tuple1)
+
+# Delete an itme by Value and remove
+print(marx_tuple1)
+marx_tuple1.remove('Adam')
+print(marx_tuple1)
+
+# Get an item by offset
+marx_tuple1.pop()
+print(marx_tuple1)
+marx_tuple1.pop(3)
+print(marx_tuple1)
+
+# Delete all item with clear()
+work_quotes = ['hard', 'questions', 'quick']
+work_quotes
+work_quotes.clear()
+print(work_quotes)
